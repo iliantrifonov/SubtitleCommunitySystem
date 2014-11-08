9bd055dbbf1069f8babcbb835757b052b0ef2be6
iliantrifonov <shogun11@abv.bg>
2014-11-08 16:34:17 +0200

    Added 3rd display template DisplayTemplates/TeamOutputModel.cshtml

--- SubtitleCommunitySystem/Web/SubtitleCommunitySystem.Web/Areas/Administration/Models/Teams/TeamOutputModel.cs
+++ SubtitleCommunitySystem/Web/SubtitleCommunitySystem.Web/Areas/Administration/Models/Teams/TeamOutputModel.cs
@@ -1,12 +1,22 @@
 ﻿namespace SubtitleCommunitySystem.Web.Areas.Administration.Models
 {
+    using AutoMapper;
+
     using SubtitleCommunitySystem.Model;
     using SubtitleCommunitySystem.Web.Infrastructure.Mappings;
 
-    public class TeamOutputModel : IMapFrom<Team>, IMapTo<Team>
+    public class TeamOutputModel : IMapFrom<Team>, IMapTo<Team>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
-        public int Name { get; set; }
+        public string Name { get; set; }
+
+        public string Language { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Team, TeamOutputModel>()
+                .ForMember(m => m.Language, opt => opt.MapFrom(u => u.Language.Name));
+        }
     }
 }
