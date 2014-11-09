4aae1e75f7883eaa45c59611263a90493966d66e
iliantrifonov <shogun11@abv.bg>
2014-11-09 11:19:40 +0200

    Added Private area

---
+++ SubtitleCommunitySystem/Web/SubtitleCommunitySystem.Web/Areas/Private/Controllers/MyRequestsController.cs
@@ -0,0 +1,18 @@
+﻿namespace SubtitleCommunitySystem.Web.Areas.Private.Controllers
+{
+    using SubtitleCommunitySystem.Web.Controllers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class MyRequestsController : AuthenticatedUserController
+    {
+        // GET: Private/MyRequests
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SubtitleCommunitySystem/Web/SubtitleCommunitySystem.Web/Controllers/AuthenticatedUserController.cs
@@ -0,0 +1,25 @@
+﻿namespace SubtitleCommunitySystem.Web.Controllers
+{
+    using SubtitleCommunitySystem.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    [Authorize]
+    public class AuthenticatedUserController : BaseController
+    {
+        public AuthenticatedUserController(IApplicationData data)
+            : base(data)
+        {
+            //TODO: MyRequests controller
+        }
+
+        // GET: AuthenticatedUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
