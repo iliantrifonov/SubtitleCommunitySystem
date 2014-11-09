6f6a8bf3a4424b4649f87a7529cc082be47bcb00
iliantrifonov <shogun11@abv.bg>
2014-11-09 17:48:09 +0200

    Added MyLanguagesController, fully functional

--- SubtitleCommunitySystem/Web/SubtitleCommunitySystem.Web/Areas/Private/Controllers/MyLanguagesController.cs
+++ SubtitleCommunitySystem/Web/SubtitleCommunitySystem.Web/Areas/Private/Controllers/MyLanguagesController.cs
@@ -1,17 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace SubtitleCommunitySystem.Web.Areas.Private.Controllers
+﻿namespace SubtitleCommunitySystem.Web.Areas.Private.Controllers
 {
-    public class MyLanguagesController : Controller
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+
+    using SubtitleCommunitySystem.Web.Areas.Administration.Models;
+    using SubtitleCommunitySystem.Web.Areas.Private.Models;
+    using SubtitleCommunitySystem.Web.Controllers;
+    using SubtitleCommunitySystem.Data;
+
+    public class MyLanguagesController : AuthenticatedUserController
     {
+        public MyLanguagesController(IApplicationData data)
+            : base(data)
+        {
+
+        }
+
         // GET: Private/MyLanguages
         public ActionResult Index()
         {
-            return View();
+            var languages = this.CurrentUser.Languages.OrderBy(l => l.Name)
+                .AsQueryable()
+                .Project().To<LanguageOutputModel>()
+                .ToList();
+
+            return View(languages);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            var languages = this.Data.Languages.All().Select(l => new SelectListItem() { Text = l.Name, Value = l.Id.ToString() });
+
+
+            var model = new AddLanguageViewModel()
+            {
+                Languages = languages
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Add(AddLanguageViewModel model)
+        {
+            var language = this.Data.Languages.Find(int.Parse(model.LanguageId));
+
+            if (language == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var alreadyHasLanguage = this.CurrentUser.Languages.Any(l => l.Id == language.Id);
+
+            if (alreadyHasLanguage)
+            {
+                return RedirectToAction("Index");
+            }
+
+            this.CurrentUser.Languages.Add(language);
+            this.Data.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ SubtitleCommunitySystem/Web/SubtitleCommunitySystem.Web/Areas/Private/Models/MyLanguages/AddLanguageViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SubtitleCommunitySystem.Web.Areas.Private.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using SubtitleCommunitySystem.Web.Areas.Administration.Models;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddLanguageViewModel
+    {
+        [Display(Name="Choose Language")]
+        public string LanguageId { get; set; }
+
+        public IEnumerable<SelectListItem> Languages { get; set; }
+    }
+}
